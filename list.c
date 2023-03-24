b580afef484f51a4d56417944c0f7289e197ff57
Replit user <>
2023-03-24 18:06:21 +0000

    new try

--- list.c
+++ list.c
@@ -117,6 +117,7 @@ void * popBack(List * list) {
 void * popCurrent(List * list) {
   if(list->current->prev!=NULL){ 
     list->current->prev->next=list->current->next;
+    list->current->next=list->current->prev;
   }
   if(list->current==list->head){
     list->head=list->head->next;
