62f2509f49e7f8b07adb0412b2487b2a14ef7934
Replit user <>
2023-03-25 10:24:05 +0000

    new try

--- list.c
+++ list.c
@@ -126,8 +126,8 @@ void * popCurrent(List * list) {
   }
   //si el current esta al principio de la lista
   if(list->current==list->head){
-    list->head=list->head->next;
-    list->head->prev=NULL;
+    list->current=list->current->next;
+    //list->head->prev=NULL;
     list->current->prev=NULL;
   }
   //retorno dato eliminado
