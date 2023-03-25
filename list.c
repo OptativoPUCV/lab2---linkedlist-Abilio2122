ef3f99558f707262a7c4166088ad59a492338240
Replit user <>
2023-03-25 10:26:16 +0000

    new try

--- list.c
+++ list.c
@@ -125,7 +125,7 @@ void * popCurrent(List * list) {
     list->current->next->prev=NULL;
   }*/
   //si el current esta al principio de la lista
-  if(list->current==list->head){
+  if(list->current->prev==NULL){
     list->current=list->current->next;
     //list->head->prev=NULL;
     list->current->prev=NULL;
